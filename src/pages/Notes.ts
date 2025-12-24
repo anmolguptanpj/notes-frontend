a62dc823f8b729934e0764e5944479fd3251f7f0
anmolguptanpj <anmolguptanpj282@gmail.com>
2025-12-24 19:43:20 +0530

    added notes note template

--- src/pages/Notes.ts
+++ src/pages/Notes.ts
@@ -1,11 +1,9 @@
+import template from "../templates/notes.template.html?raw"
+
 export default function Notes(): HTMLElement{
     const container = document.createElement("div");
 
-    container.innerHTML = `
-    <h1>Notes</h1>
-    <p>This is my first vanilla TS SPA page </p>  
-    <button id="go-notes">Go to Homes</button>
-    `;
+    container.innerHTML =template
 
     return container
     
