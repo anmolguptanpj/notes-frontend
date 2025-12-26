e47e20f0c637d5eaf982a08e976b5480d8affe0e
anmolguptanpj <anmolguptanpj282@gmail.com>
2025-12-26 17:08:41 +0530

    appending and using event listeners.html

--- src/pages/Notes.ts
+++ src/pages/Notes.ts
@@ -3,7 +3,38 @@ import template from "../templates/notes.template.html?raw"
 export default function Notes(): HTMLElement{
     const container = document.createElement("div");
 
-    container.innerHTML =template
+    container.innerHTML = template
+
+    let todos=[];
+
+    function saveTodos(value : string){
+        todos.push(value)
+    }
+
+ function renderTodos(){
+    const todo = container.querySelector("#content")
+   
+    todos.forEach((item)=>{
+         const div = document.createElement("div")
+         div.innerHTML=item
+         todo.appendChild(div)
+
+    })
+
+    
+ }
+
+    const input = container.querySelector<HTMLInputElement>("#input")
+    input?.addEventListener("input",(e)=>{
+        const value = (e.target as HTMLInputElement).value;
+        console.log(value)
+       if(!value)return;
+       saveTodos(value)
+       renderTodos()
+
+    })
+
+
 
     return container
     
